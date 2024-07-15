5c3301e46688200807cc588adc2d3884d50e2434
liron28 <liron.d28@gmail.com>
2024-07-15 16:19:24 +0300

    first commit

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,85 @@
+import { createContext,useState, useEffect } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider =({children}) =>{
+    const[isLoading, setIsLoading] = useState(true);
+    const [feedback,setFeedback]= useState([]);
+    //state
+    const [feedbackEdit,setFeedbackEdit]= useState({
+        item:{},
+        edit: false
+    });
+
+    useEffect(()=>{
+        fetchFeedback();
+    },[])
+
+    //Fetch feedback
+    const fetchFeedback= async () => {
+        const response = await fetch(`/feedback`);
+        const data = await response.json();
+        console.log(data);
+        setFeedback(data);
+        setIsLoading(false);
+    }
+
+    //Delete feedback
+    const deleteFeedback = async (id) =>{
+        if(window.confirm('Are you sure you want to delete?')){
+            await fetch(`/feedback/${id}`, {method: 'DELETE'})
+            setFeedback(feedback.filter((item) =>item.id !==id))
+        }
+    }
+    //Update the data
+    const updateFeedback =async (id,updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updItem),
+        })
+
+        const data =  await response.json()
+        setFeedback(
+            feedback.map((item)=>item.id === id? {...item,...data} : item))
+    }
+
+    // Add feedback
+    const addFeedback =async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback),
+        });
+        const data = await response.json();
+
+        setFeedback([data,...feedback]);
+    }
+    //Set item to be updated (function)
+    const editFeedback=(item)=>{
+        setFeedbackEdit({
+            item,
+            edit: true
+        });
+    }
+
+    return <FeedbackContext.Provider 
+        value={{
+            feedback,
+            feedbackEdit,
+            isLoading,
+            deleteFeedback,
+            addFeedback,
+            editFeedback,
+            updateFeedback,
+            
+        }}>
+        {children}
+    </FeedbackContext.Provider>
+}
+
+export default FeedbackContext;
